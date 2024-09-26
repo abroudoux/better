feca783fe14fee603347370f90e45ab56d614d8c
arthur <arthur.broudoux@gmail.com>
2024-09-26 15:35:45 +0200

    feat: update habits services Signed-off-by: Arthur Broudoux <arthur.broudoux@gmail.com>

--- src/lib/db/seed.ts
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-
-import { db, connection } from "$lib/db/client";
-import { habits } from "$lib/db/schema";
-import { habitsTest } from "$utils/constants";
-
-await db.insert(habits).values(habitsTest).execute();
-
-await connection.end();
