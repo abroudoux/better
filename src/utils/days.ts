1f741087dc5f2c7d76bdb32371dd42df1f65382b
arthur <arthur.broudoux@gmail.com>
2024-11-16 14:58:12 +0100

    refactor(days.ts): delete unusued const assignations Signed-off-by: Arthur Broudoux <arthur.broudoux@gmail.com>

--- src/utils/days.ts
+++ src/utils/days.ts
@@ -1,6 +1,6 @@
 import { isNewDay, postNewDay, putDay } from "$services/days.services";
 import { getAllHabits, toggleAllHabitsStatus } from "$services/habits.services";
-import type { Day, Habit } from "$utils/types/entities";
+import type { Habit } from "$utils/types/entities";
 
 export function getDate() {
 	return new Date().toISOString().slice(0, 10);
@@ -11,10 +11,10 @@ export async function manageDay() {
 	const habits: Habit[] = await getAllHabits(fetch);
 
 	if (response.isNewDay) {
-		const dayCreated: Day = await postNewDay(fetch, habits);
-		const habitsUpdated: Habit[] = await toggleAllHabitsStatus(fetch, habits);
+		await postNewDay(fetch, habits);
+		await toggleAllHabitsStatus(fetch, habits);
 		window.location.reload();
 	} else if (response.dayId) {
-		const dayUpdated: Day = await putDay(fetch, response.dayId, habits);
+		await putDay(fetch, response.dayId, habits);
 	}
 }
